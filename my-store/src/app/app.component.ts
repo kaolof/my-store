e59e9203e03f2c52211b04a2378d6ed7fb04c275
Kao Lo <kaochilanlo@gmail.com>
2023-01-11 21:05:12 -0400

    basics in angular

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'kao';
+  age=18;
+  img='https://source.unsplash.com/random';
+  btnDisabled='true';
+  //object
+  person={
+    name: 'Nicolas',
+    age: 18,
+    avatar:'https://source.unsplash.com/random'
+  }
 }
