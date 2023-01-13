69091af90166f5277ef5a32793ee120bd6ff87b2
Kao Lo <kaochilanlo@gmail.com>
2023-01-13 00:17:56 -0400

    events

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -3,21 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'kao';
   age=18;
   img='https://source.unsplash.com/random';
-  btnDisabled='true';
+  btnDisabled=true;
   //object
   person={
     name: 'Nicolas',
     age: 18,
     avatar:'https://source.unsplash.com/random'
   }
-
+  toggleButton(){
+    this.btnDisabled=!this.btnDisabled;
+  }
+  increaseAge(){
+    this.person.age+=1;
+  }
   onScroll(event:Event){
-    
+    const element=event.target as HTMLElement;
+    console.log(element.scrollTop);
   }
 }
